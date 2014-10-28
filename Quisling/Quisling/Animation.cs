09af5b550688b3f9686b3b3e40cedbdee6a5b02e
shibinv@gmail.com <shibinv@gmail.com>
2014-10-28 11:01:45 -0500

    testing upload to git

---
+++ Quisling/Quisling/Animation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quisling
+{
+    class Animation
+    {
+        Texture2D spriteSheet;
+        float scale;
+        int elapsedTime;
+        int frameTime;
+        Point frameCount;
+        Point currentFrame;
+        Color color;
+        Rectangle sourceRect = new Rectangle();
+        Rectangle destRect = new Rectangle();
+        public bool Active;
+        public bool Looping;
+        public Vector2 Position;
+
+
+        public void Initalize()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ Quisling/Quisling/Background.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quisling
+{
+    class Background
+    {
+        Texture2D texture;
+        Vector2[] position;
+        int speed;
+
+        public void Initalize(ContentManager content, String texturePath, int screenWidth, int speed)
+        {
+
+            texture = content.Load<Texture2D>(texturePath);
+
+            this.speed = speed;
+
+            position = new Vector2[screenWidth / texture.Width + 2];
+
+            for (int i = 0; i < position.Length; i++)
+            {
+                position[i] = new Vector2(i * texture.Width, 0);
+            }
+
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < position.Length; i++)
+            {
+                position[i].X += speed;
+               
+                if (speed <= 0)
+                {
+                    if (position[i].X <= -texture.Width)
+                    {
+                        position[i].X = texture.Width * (position.Length - 1);
+                    }
+                }
+                else
+                {
+                    if (position[i].X >= texture.Width * (position.Length - 1))
+                    {
+                        position[i].X = -texture.Width;
+                    }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < position.Length; i++)
+            {
+                spriteBatch.Draw(texture, position[i], Color.White);
+            }
+        }
+    }
+}
