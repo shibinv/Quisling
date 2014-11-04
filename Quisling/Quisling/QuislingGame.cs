0c0d42cd80729625c832431364dda5b4ec644fdc
shibinv@gmail.com <shibinv@gmail.com>
2014-11-04 11:50:39 -0600

    Touch ups to demo level.

--- Quisling/Quisling/QuislingGame.cs
+++ Quisling/Quisling/QuislingGame.cs
@@ -144,7 +144,7 @@ namespace Quisling {
         private void LoadNextLevel() {
             // move to the next level
             levelIndex = (levelIndex + 1) % numberOfLevels;
-            //levelIndex = 0;
+            //levelIndex = 5;
 
             // Unloads the content for the current level before loading the next one.
             if (level != null)
